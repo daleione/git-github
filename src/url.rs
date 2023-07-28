39da90f2d666c6f02e4210ad419132d05367dbe5
dalei <guoyunlei@live.com>
2023-07-28 16:21:02 +0800

    test: remote url

--- src/url.rs
+++ src/url.rs
@@ -5,24 +5,6 @@ use nom::bytes::complete::{tag, take_till, take_until, take_while};
 use nom::sequence::{terminated, tuple};
 use nom::IResult;
 
-enum Platform {
-    Github,
-    Gitlab,
-    Other(String),
-}
-
-/// Contains the address of the git repository.
-/// git remote url can be two format:
-/// * git@xxx.com:user/repo.git
-//  * https://xxx.com/user/repo.git
-#[derive(Debug, Default)]
-pub struct Remote {
-    schema: String,
-    host: String,
-    user: String,
-    repo: String,
-}
-
 fn schema_parser(input: &str) -> IResult<&str, &str> {
     terminated(
         alt((tag("git"), tag("https"), tag("http"))),
@@ -43,6 +25,24 @@ fn user_parser(input: &str) -> IResult<&str, &str> {
 
 fn repo_parser(input: &str) -> IResult<&str, &str> {
     take_till(|c| c == '.')(input)
+}
+
+enum Platform {
+    Github,
+    Gitlab,
+    Other(String),
+}
+
+/// Contains the address of the git repository.
+/// git remote url can be two format:
+/// * git@xxx.com:user/repo.git
+//  * https://xxx.com/user/repo.git
+#[derive(Debug, Default)]
+pub struct Remote {
+    schema: String,
+    host: String,
+    user: String,
+    repo: String,
 }
 
 impl Remote {
@@ -96,5 +96,67 @@ impl Remote {
 
     pub fn get_branch_url(&self, branch: &str) -> String {
         format!("{}/tree/{}", self.get_repo_url(), branch)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn remote_git() {
+        let remote = Remote::parse("git@xxx.com:user/repo.git");
+        assert!(remote.is_some());
+        if let Some(remote) = remote {
+            assert!(remote.is_git());
+            assert_eq!(remote.schema, "git");
+            assert_eq!(remote.repo, "repo");
+            assert_eq!(remote.host, "xxx.com");
+            assert_eq!(remote.user, "user");
+            assert!(matches!(remote.get_platform(), Platform::Other(..)));
+        }
+    }
+
+    #[test]
+    fn remote_http() {
+        let remote = Remote::parse("http://github.com/user/repo.git");
+        assert!(remote.is_some());
+        if let Some(remote) = remote {
+            assert!(remote.is_http());
+            assert_eq!(remote.schema, "http");
+            assert_eq!(remote.repo, "repo");
+            assert_eq!(remote.host, "github.com");
+            assert_eq!(remote.user, "user");
+            assert!(matches!(remote.get_platform(), Platform::Github));
+        }
+    }
+
+    #[test]
+    fn remote_https() {
+        let remote = Remote::parse("https://xxx.com/user/repo.git");
+        assert!(remote.is_some());
+        if let Some(remote) = remote {
+            assert!(remote.is_http());
+            assert_eq!(remote.schema, "https");
+            assert_eq!(remote.repo, "repo");
+            assert_eq!(remote.host, "xxx.com");
+            assert_eq!(remote.user, "user");
+        }
+    }
+
+    #[test]
+    fn remote_url() {
+        let remote = Remote::parse("https://github.com/user/repo.git");
+        if let Some(remote) = remote {
+            assert_eq!(remote.get_repo_url(), "https://github.com/user/repo");
+            assert_eq!(
+                remote.get_commit_url("commit_id"),
+                "https://github.com/user/repo/commit/commit_id",
+            );
+            assert_eq!(
+                remote.get_branch_url("test"),
+                "https://github.com/user/repo/tree/test",
+            );
+        }
     }
 }
